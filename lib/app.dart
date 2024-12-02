01ed4a6f0968fb7034b00b65fda3fe9796fb5e9a
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2024-12-02 22:52:00 +0600

    first push

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/images/png/Welcome.png",
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+      ),
+    );
+  }
+}
