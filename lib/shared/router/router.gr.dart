a3b04f753f4d42b2a360a0c0e7c70866cb036ffb
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2024-12-15 02:21:47 +0600

    added bottom nav bar

--- lib/shared/router/router.gr.dart
+++ lib/shared/router/router.gr.dart
@@ -8,15 +8,19 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:movie_app/features/auth/presentation/sign_in_page.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i7;
+import 'package:movie_app/features/auth/presentation/sign_in_page.dart' as _i5;
 import 'package:movie_app/features/home/presentation/pages/home_page.dart'
     as _i1;
+import 'package:movie_app/features/movie_screen.dart' as _i3;
+import 'package:movie_app/main_screen.dart' as _i2;
+import 'package:movie_app/profile_page.dart' as _i4;
+import 'package:movie_app/ticket_page.dart' as _i6;
 
 /// generated route for
 /// [_i1.HomePage]
-class HomeRoute extends _i3.PageRouteInfo<void> {
-  const HomeRoute({List<_i3.PageRouteInfo>? children})
+class HomeRoute extends _i7.PageRouteInfo<void> {
+  const HomeRoute({List<_i7.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -24,7 +28,7 @@ class HomeRoute extends _i3.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static _i3.PageInfo page = _i3.PageInfo(
+  static _i7.PageInfo page = _i7.PageInfo(
     name,
     builder: (data) {
       return const _i1.HomePage();
@@ -33,9 +37,66 @@ class HomeRoute extends _i3.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i2.SignInPage]
-class SignInRoute extends _i3.PageRouteInfo<void> {
-  const SignInRoute({List<_i3.PageRouteInfo>? children})
+/// [_i2.MainScreen]
+class MainRoute extends _i7.PageRouteInfo<void> {
+  const MainRoute({List<_i7.PageRouteInfo>? children})
+      : super(
+          MainRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MainRoute';
+
+  static _i7.PageInfo page = _i7.PageInfo(
+    name,
+    builder: (data) {
+      return const _i2.MainScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i3.MovieScreen]
+class MovieRoute extends _i7.PageRouteInfo<void> {
+  const MovieRoute({List<_i7.PageRouteInfo>? children})
+      : super(
+          MovieRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MovieRoute';
+
+  static _i7.PageInfo page = _i7.PageInfo(
+    name,
+    builder: (data) {
+      return const _i3.MovieScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i4.ProfilePage]
+class ProfileRoute extends _i7.PageRouteInfo<void> {
+  const ProfileRoute({List<_i7.PageRouteInfo>? children})
+      : super(
+          ProfileRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ProfileRoute';
+
+  static _i7.PageInfo page = _i7.PageInfo(
+    name,
+    builder: (data) {
+      return const _i4.ProfilePage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i5.SignInPage]
+class SignInRoute extends _i7.PageRouteInfo<void> {
+  const SignInRoute({List<_i7.PageRouteInfo>? children})
       : super(
           SignInRoute.name,
           initialChildren: children,
@@ -43,10 +104,29 @@ class SignInRoute extends _i3.PageRouteInfo<void> {
 
   static const String name = 'SignInRoute';
 
-  static _i3.PageInfo page = _i3.PageInfo(
+  static _i7.PageInfo page = _i7.PageInfo(
     name,
     builder: (data) {
-      return const _i2.SignInPage();
+      return const _i5.SignInPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i6.TicketPage]
+class TicketRoute extends _i7.PageRouteInfo<void> {
+  const TicketRoute({List<_i7.PageRouteInfo>? children})
+      : super(
+          TicketRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'TicketRoute';
+
+  static _i7.PageInfo page = _i7.PageInfo(
+    name,
+    builder: (data) {
+      return const _i6.TicketPage();
     },
   );
 }
--- lib/shared/theme/theme.dart
+++ lib/shared/theme/theme.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/shared/theme/app_colors.dart';
 
 ThemeData theme = ThemeData(
-    fontFamily: "Kanit-Regular",
-    appBarTheme: const AppBarTheme(color: AppColors.splashBlackColor),
-    scaffoldBackgroundColor: AppColors.splashBlackColor,
-    textSelectionTheme: const TextSelectionThemeData(
-      selectionColor: AppColors.yellow,
-      cursorColor: AppColors.yellow,
-      selectionHandleColor: AppColors.yellow,
-    ));
+  fontFamily: "Kanit-Regular",
+  appBarTheme: const AppBarTheme(color: AppColors.splashBlackColor),
+  scaffoldBackgroundColor: AppColors.splashBlackColor,
+  textSelectionTheme: const TextSelectionThemeData(
+    selectionColor: AppColors.yellow,
+    cursorColor: AppColors.yellow,
+    selectionHandleColor: AppColors.yellow,
+  ),
+);
